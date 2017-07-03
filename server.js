0f211fb162ad09f847e7815bca683fa68029cfc2
davidmball <davidmichaelball@gmail.com>
2017-07-03 23:45:53 +0200

    Added configuration files.

--- server.js
+++ server.js
@@ -6,25 +6,30 @@ const io   = require('socket.io')(http);
 const dir = require('node-dir');
 const bodyParser = require('body-parser');
 const crypto = require("crypto");
+const config = require('config');
 
 var app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+var example_path = config.get('example_path');
+var compile_path = config.get('compile_path');
+var host_port = config.get('host_port');
+var host = config.get('host');
+var rosbridge_port = config.get('rosbridge_port');
+
+var example_path_name = 'topics';
 var example_name = 'chatter';
+
+
 var files = {};
-var example_path = '/home/david/ros_examples/src/topics/';
-
-
-
 dir.readFiles(example_path,
     function(err, content, filename, next) {
         if (err)
             throw err;
-        files[filename.split("/").pop()] = content;
-  //      console.log('filename:', filename);
-    //    console.log('content:', content);
+
+        files[filename.replace(example_path + example_path_name, '')] = content;
         next();
     },
     function(err, files){
@@ -45,6 +50,9 @@ app.get('/chatter.html', function (req, res) {
         } else {
             res.contentType('text/html');
             data = data.toString().replace(/\{\{example_title\}\}/, example_name);
+            data = data.toString().replace(/\{\{host_name\}\}/, host);
+            data = data.toString().replace(/\{\{rosbridge_port\}\}/, rosbridge_port);
+
             res.send(data);
         }
     });
@@ -54,7 +62,7 @@ app.get("/get_files", function (req, res) {
   res.send(JSON.stringify(files));
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(host_port, function () {
    var host = server.address().address;
    var port = server.address().port;
 
@@ -64,19 +72,22 @@ var server = app.listen(8080, function () {
 var socket = io.listen(server);
 
 
+
 app.post('/compile', function(req, res)
 {
     console.log("Writing code.");
-    fs.writeFileSync('/home/david/catkin_ws/src/topics/CMakeLists.txt', files['CMakeLists.txt']);
-    fs.writeFileSync('/home/david/catkin_ws/src/topics/package.xml', files['package.xml']);
-    fs.writeFileSync('/home/david/catkin_ws/src/topics/src/chatter.cpp', req.body.code_files['chatter.cpp']);
-
+    fs.writeFileSync(compile_path + example_path_name + 'CMakeLists.txt', files['CMakeLists.txt']);
+    fs.writeFileSync(compile_path + example_path_name + 'package.xml', files['package.xml']);
+    for (var filename in req.body.code_files)
+    {
+      fs.writeFileSync(compile_path + example_path_name + filename, req.body.code_files[filename]);
+    }
     console.log("Compiling code.");
     var spawn = require('child_process').spawn;
     var command = './run_docker.sh'
     var docker_id = crypto.randomBytes(16).toString("hex");
     var net_id = crypto.randomBytes(16).toString("hex");
-    var args = ['10', docker_id, 'ros_workspace_rosbridge', '/home/david/catkin_ws/src', net_id];
+    var args = ['10', docker_id, 'ros_workspace_rosbridge', compile_path, net_id];
     var ls = spawn(command, args);
 
     ls.stdout.on('data', (data) => {
