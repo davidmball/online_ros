cf524639a9ef2d6f473f305c2adad482f646b019
davidmball <davidmichaelball@gmail.com>
2017-08-23 21:57:35 +0200

    Hiding rosbridge error

--- dockerfile/kinetic/ros_entrypoint.sh
+++ dockerfile/kinetic/ros_entrypoint.sh
@@ -6,8 +6,8 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 roscore &
 stdbuf -o L catkin build --no-status &&
 (source "/home/root/catkin_ws/devel/setup.bash" &&
-(roslaunch rosbridge_server rosbridge_websocket.launch &
-rosrun tf2_web_republisher tf2_web_republisher > /dev/null 2>&1 &
+(roslaunch rosbridge_server rosbridge_websocket.launch &> /dev/null &
+rosrun tf2_web_republisher tf2_web_republisher &> /dev/null &
 until stdbuf -oL rosnode list | grep -m 1 "/rosbridge_websocket"; do sleep 0.2; done &&
 (exec "$@")
 ))
