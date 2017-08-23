a6e688e27a2853ffbb7eeb29b27c2cbcedf37359
davidmball <davidmichaelball@gmail.com>
2017-08-23 22:30:01 +0200

    Removing rosnode error message

--- dockerfile/kinetic/ros_entrypoint.sh
+++ dockerfile/kinetic/ros_entrypoint.sh
@@ -8,6 +8,6 @@ stdbuf -o L catkin build --no-status &&
 (source "/home/root/catkin_ws/devel/setup.bash" &&
 (roslaunch rosbridge_server rosbridge_websocket.launch &> /dev/null &
 rosrun tf2_web_republisher tf2_web_republisher &> /dev/null &
-until stdbuf -oL rosnode list | grep -m 1 "/rosbridge_websocket"; do sleep 0.2; done &&
+until stdbuf -oL rosnode list 2>/dev/null | grep -m 1 "/rosbridge_websocket"; do sleep 0.2; done &&
 (exec "$@")
 ))
